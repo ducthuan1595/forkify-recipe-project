8da9fd250a652c8844c25c25199ae03df792524b
thuantruong <hi.tim.gi.the@gmail.com>
2023-08-04 00:40:00 +0700

    complely

--- src/js/config.js
+++ src/js/config.js
@@ -1,2 +1,5 @@
-export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes';
-export const TiMEOUT = 10;
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '6fc8248b-14aa-4427-9f65-882261e11420';
+export const MODAL_CLOSE_SEC = 2.5;
